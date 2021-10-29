0ad9b98317d0c4f352a1bf3f44835b3c5ffb0e9e
GSV78 <gradov78@gmail.com>
2021-10-29 13:27:41 +0300

    Finished styles+JS of 5th screen

--- js/main.js
+++ js/main.js
@@ -7,4 +7,13 @@ $(function () {
         nextArrow: '<button class="slick-next"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.21839 1L1 9L9.21839 17"/></svg></button>',
     });
 
+
+    $('.questions__item-title').on('click', function(){
+
+        $('.questions__item').removeClass('questions__item--active');
+        $(this).parent().addClass('questions__item--active');
+        
+    })
+
+
 });
