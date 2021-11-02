d86f2eb3091e2dc44a6a353817296ae0e0e9a309
GSV78 <gradov78@gmail.com>
2021-11-02 19:59:51 +0300

    Redisigned 3th screen style

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 $(function () {
 
     $('.products__slider').slick({
-        autoplay: true,
         infinite: false,
         prevArrow: '<button class="slick-prev"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.78161 17L9 9L0.78161 1"/></svg></button>',
         nextArrow: '<button class="slick-next"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.21839 1L1 9L9.21839 17"/></svg></button>',
@@ -15,5 +14,11 @@ $(function () {
         
     })
 
+    $('#fullpage').fullpage({
+        sectionSelector: '.page-section',
+        scrollOverflow: true,
+        anchors: ['top', 'products', 'benefits', 'specification', 'questions', 'contacts'],
+        menu: '#header__nav',
+	});
 
 });
